4326884ca49a3a0f11b82e39109ef61d1277e343
Pascal <niedermeyer.pascal@gmail.com>
2018-11-20 13:25:08 +0200

    Working List View from array

--- app/src/main/java/com/example/pascal/shopnavigator/ShoppingBag.java
+++ app/src/main/java/com/example/pascal/shopnavigator/ShoppingBag.java
@@ -8,9 +8,18 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
 
 public class ShoppingBag extends SceneParent implements View.OnClickListener{
+
+    // Array of strings...
+    ListView simpleList;
+    String countryList[] = {"Salt", "Apples", "Sugar", "Bread", "Milk"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,10 @@ public class ShoppingBag extends SceneParent implements View.OnClickListener{
         //Button to next View
         Button button1 = (Button) findViewById(R.id.go_shop_btn);
         button1.setOnClickListener((View.OnClickListener) this);
+
+        simpleList = (ListView)findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_list_view, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
 
     }
 
