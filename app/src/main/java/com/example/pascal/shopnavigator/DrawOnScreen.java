5c851ea8dd9754156c58d52e2725e8ac50070a73
Tobias Reinert <TobReinert@web.de>
2018-12-18 01:59:06 +0100

    Routing Algorithm, Drawing Algorithm

--- app/src/main/java/com/example/pascal/shopnavigator/DrawOnScreen.java
+++ app/src/main/java/com/example/pascal/shopnavigator/DrawOnScreen.java
@@ -7,7 +7,16 @@ import android.graphics.Paint;
 import android.view.View;
 
 public class DrawOnScreen extends View {
+    Paint paint = new Paint();
     int[][] checkPoints = new int[26][2];
+    Canvas canvas = new Canvas();
+
+
+    public DrawOnScreen(Context context) {
+        super(context);
+        paint.setColor(Color.RED);
+    }
+
 
     //SetupCheckpoints;
     public void setPoints() {
@@ -21,38 +30,61 @@ public class DrawOnScreen extends View {
 
     public void rowOfPoints(int y, int start, int stop, int multiplier) {
         for (int i = start; i <= stop; i++) {
-            checkPoints[i-1][0] = 8*multiplier;
-            checkPoints[i-1][1] = y;
+            checkPoints[i][0] = 8*multiplier;
+            checkPoints[i][1] = y;
             multiplier++;
         }
     }
 
 
-    Paint paint = new Paint();
-
-    public DrawOnScreen(Context context) {
-        super(context);
-        paint.setColor(Color.RED);
-    }
-
     public void PrepareRoute(int [][] shortestPath) {
-        Canvas canvas = new Canvas();
-        int startX = 0, startY = 0, stopX = 0, stopY = 0;
+        setPoints();
+        int [][] destinationCheckpoint = new int [1][2];
+        int nextX = 0, nextY = 0, stopX = 0, stopY = 0, startX = 0, startY = 0;
 
-        for (int i = 0; i < shortestPath.length-1; i++) {
-            startX = shortestPath[i][0];
-            startY = shortestPath[i][1];
+        DrawLine(checkPoints[0][0], checkPoints[0][1], checkPoints[1][0], checkPoints[1][1]);
 
-            stopX  = shortestPath[i+1][0];
-            stopY  = shortestPath[i+1][0];
+        startX = checkPoints[1][0];
+        startY = checkPoints[1][1];
 
-
-            DrawRoute(canvas, startX, startY, stopX, stopY);
+        for (int i = 1; i < shortestPath.length-1; i++) {
+            nextX = shortestPath[i][0];
+            nextY = shortestPath[i][1];
+            destinationCheckpoint = findClosestCheckpoint(nextX, nextY);
+            DrawLine(startX, startY, destinationCheckpoint[0][0], startY);
+            DrawLine(destinationCheckpoint[0][0], startY, destinationCheckpoint[0][0], destinationCheckpoint[0][1]);
+            startX = destinationCheckpoint[0][0];
+            startY = destinationCheckpoint[0][1];
         }
     }
 
-    public void DrawRoute(Canvas canvas, int startX, int startY, int stopX, int stopY) {
+    public void DrawLine(int startX, int startY, int stopX, int stopY) {
         canvas.drawLine(startX, startY, stopX, stopY, paint);
+    }
+
+    public int [][] findClosestCheckpoint(int startX, int startY){
+        int [][] destinationPoint = new int [1][2];
+        int distanceX = 100;
+        int distanceY = 100;
+        int holdX = 0;
+        int holdY = 0;
+
+        for (int i = 1; i < 26; i++) {
+            if (Math.abs(startX - checkPoints[i][0]) < distanceX) {
+                distanceX = Math.abs(startX - checkPoints[i][0]);
+                holdX = checkPoints[i][0];
+            }
+
+            if (Math.abs(startY - checkPoints[i][1]) < distanceY) {
+                distanceY = Math.abs(startX - checkPoints[i][1]);
+                holdY = checkPoints[i][1];
+            }
+        }
+        destinationPoint[0][0] = holdX;
+        destinationPoint[0][1] = holdY;
+        //destinationPoint = holdX;
+
+        return destinationPoint;
     }
 
 }
