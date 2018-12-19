a7749026e1f8141c61bfffbdadd39453ed2904fe
BlackReaper94 <tobReinert@web.de>
2018-12-19 07:03:41 +0100

    Fixed routing algorithm

--- app/src/main/java/com/example/pascal/shopnavigator/Routing.java
+++ app/src/main/java/com/example/pascal/shopnavigator/Routing.java
@@ -7,7 +7,7 @@ public class Routing {
         // Variables
         int [][] shortestPath = new int [gridPosition.length+2][2];
         int totalDistance = 1000;
-        int temp = 0;
+        int temp = 10000;
         int current = 0;
         int marker = 0;
         final int entranceX = 10;
@@ -15,44 +15,42 @@ public class Routing {
         final int cashiersX = 55;
         final int cashiersY = 80;
 
+        for (int i = 0; i < shortestPath.length; i++) {
+            shortestPath[i][0] = 1000;
+            shortestPath[i][1] = 2000;
+        }
+
         //Start and End value (Entrance and Cashiers) are static and will be added automatically;
         shortestPath[0][0] = entranceX;
         shortestPath[0][1] = entranceY;
         shortestPath[shortestPath.length-1][0] = cashiersX;
         shortestPath[shortestPath.length-1][1] = entranceY;
 
-        if(gridPosition.length == 1) {
-            shortestPath[1][0] = gridPosition[0][0];
-            shortestPath[1][1] = gridPosition[0][1];
-        } else {
-            for (int e = 1; e < gridPosition.length-1; e++) {
-                for (int i = 1; i < gridPosition.length - 1; i++) {
-                    System.out.println(shortestPath[1][0]);
-                    if (i != current) {
-                        temp = (Math.abs(gridPosition[i][0] - gridPosition[current][0])) + Math.abs((gridPosition[i][1] - gridPosition[current][1]));
+        for (int i = 0; i < gridPosition.length; i++) {
+            for (int e = 0; e < gridPosition.length; e++) {
+                if(current != i) {
+                    temp = (Math.abs(shortestPath[e][0] - gridPosition[current][0]) + Math.abs(shortestPath[e][1] - gridPosition[current][1]));
+                } else {
 
-                    } else {
-                        shortestPath[e][0] = gridPosition[current][0];
-                        shortestPath[e][1] = gridPosition[current][1];
-                    }
-
-                    if (temp < totalDistance) {
-                        marker = i;
-                        totalDistance = temp;
-                    }
                 }
 
-                System.out.println(shortestPath[1][0]);
-                shortestPath[e][0] = gridPosition[marker][0];
-                shortestPath[e][1] = gridPosition[marker][1];
+                if (temp <= totalDistance) {
+                    marker = e;
+                    totalDistance = temp;
+                }
+            }
+                    shortestPath[i + 1][0] = gridPosition[marker][0];
+                    shortestPath[i + 1][1] = gridPosition[marker][1];
 
-                gridPosition[current][0] = 1000;
-                gridPosition[current][1] = 1000;
+                    if(i > 0) {
+                        gridPosition[current][0] = 1000;
+                        gridPosition[current][1] = 1000;
+                    }
 
                 current = marker;
-                totalDistance = 1000;
+
             }
-        }
+            totalDistance = 1000;
         return shortestPath;
     }
 }
