bdddd6e048f8595a1212d76bd3c881237977b039
BlackReaper94 <tobReinert@web.de>
2018-12-19 18:48:40 +0100

    Scheiße aufräumen

--- app/src/main/java/com/example/pascal/shopnavigator/DrawOnScreen.java
+++ app/src/main/java/com/example/pascal/shopnavigator/DrawOnScreen.java
@@ -15,45 +15,74 @@ public class DrawOnScreen extends SceneParent{
     private Canvas canvas = new Canvas();
     private Bitmap mBitmap;
     private ImageView mImageView;
-    int width = 0, height = 0;
-    private int[][] shortestPath;
+    private int width = 0, height = 0;
 
 
     //SetupCheckpoints;
     public void setPoints() {
-        int temp = 0;
-        // Shelf points;
-        rowOfPoints(84,0,11);
+        int [] xCoordinates = new int [8];
+        int [] yCoordinates = new int [6];
 
-        rowOfPoints(78,1,11);
-       // rowOfPoints(78,2,24);
-       // rowOfPoints(78,3,10);
+        //Initialize arrays
+        xCoordinates [0] = 11;
 
-        //rowOfPoints(42,9,11);
-       // rowOfPoints(42,9,24);
-       // rowOfPoints(42,11,10);
-
-      //  rowOfPoints(25,17,11);
-      //  rowOfPoints(25,17,24);
-      //  rowOfPoints(25,19,10);
-      //  rowOfPoints(22,21,10);
-
-
-        for (int i = 0; i < checkPoints.length; i++) {
-            // System.out.println(shortestPath[i][0] + " Befire "+ shortestPath[i][1]);
-            temp = ((width / 100) * checkPoints[i][0]);
-            checkPoints[i][0] = (int) temp;
-
-            temp = ((height / 100) * checkPoints[i][1]);
-            checkPoints[i][1] = (int) temp;
+        for (int i = 1; i < 8; i++) {
+            xCoordinates [i] = 24 + 12*(i-1);
         }
-        DrawLine(checkPoints[0][0], checkPoints[0][1], checkPoints[1][0], checkPoints[1][1]);
+
+        yCoordinates [0] = 84;
+        yCoordinates [1] = 78;
+        yCoordinates [2] = 42;
+        yCoordinates [3] = 12;
+        yCoordinates [4] = 7;
+
+        //Write checkpoints
+        int temp = 0;
+        rowOfPoints(yCoordinates[0], temp, xCoordinates[0]);
+        temp++;
+        for (int i = 0; i < 8; i++) {
+            rowOfPoints(yCoordinates[1], temp, xCoordinates[i]);
+            temp++;
+            rowOfPoints(yCoordinates[2], temp, xCoordinates[i]);
+            temp++;
+        }
+        for (int i = 0; i < 4; i++) {
+            rowOfPoints(yCoordinates[3], temp, xCoordinates[i]);
+            temp++;
+        }
+        for (int i = 3; i < 8; i++) {
+            rowOfPoints(yCoordinates[4], temp, xCoordinates[i]);
+            temp++;
+        }
+
+        //Convert to screen Value
+        checkPoints = percentValueToScreenValue(checkPoints);
+
+        //Draw Test line
+        //for (int i = 0; i < checkPoints.length-1; i++) {
+        //    DrawLine(checkPoints[i][0], checkPoints[i][1], checkPoints[i + 1][0], checkPoints[i + 1][1]);
+        //}
     }
+
 
     //Symatically build checkpoints
     public void rowOfPoints(int y, int i, int x) {
             checkPoints[i][0] = x;
             checkPoints[i][1] = y;
+    }
+
+
+    public int [][] percentValueToScreenValue(int points [][]) {
+        int temp = 0;
+
+        for (int i = 0; i < points.length; i++) {
+            temp = ((width / 100) * points[i][0]);
+            points[i][0] = (int) temp;
+
+            temp = ((height / 100) * points[i][1]);
+            points[i][1] = (int) temp;
+        }
+        return points;
     }
 
 
@@ -65,18 +94,17 @@ public class DrawOnScreen extends SceneParent{
         mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         mImageView.setImageBitmap(mBitmap);
         canvas = new Canvas(mBitmap);
-        paint.setStrokeWidth(20);
+        paint.setStrokeWidth(17);
         paint.setColor(Color.RED);
-       // DrawLine(0,0,2,2);
-        this.shortestPath = shortestPath;
+
         setPoints();
-       // PrepareRoute();
+        PrepareRoute(shortestPath);
 
         shopView.invalidate();
     }
 
     //Calculating and drawing the route
-    public void PrepareRoute() {
+    public void PrepareRoute (int [][] shortestPath) {
         double temp = 0;
         int [][] destinationCheckpoint = new int [1][2];
         int nextX = 0, nextY = 0, stopX = 0, stopY = 0, startX = 0, startY = 0;
@@ -86,28 +114,26 @@ public class DrawOnScreen extends SceneParent{
 
         //Set startposition to first checkpoint
         startX = checkPoints[1][0];
-        startY = checkPoints[1][1];
-        //System.out.println(width +" scheipsj "+ height);
+        startY = checkPoints[1][1];;
 
 
         //Convert percent values to screenvalue
-        for (int i = 0; i < shortestPath.length; i++){
-           // System.out.println(shortestPath[i][0] + " Befire "+ shortestPath[i][1]);
-            temp = ((width / 100) * shortestPath[i][0]);
-            shortestPath[i][0] = (int)temp;
+        shortestPath = percentValueToScreenValue(shortestPath);
 
-            temp = ((height / 100) * shortestPath[i][1]);
-            shortestPath[i][1] = (int)temp;
-
-          //  System.out.println(shortestPath[i][0] + " "+ shortestPath[i][1]);
-        }
-
-        //Drawing the lines between the checkpoints
+        //Drawing the lines between the checkpoints closest to the next product
         for (int i = 1; i < shortestPath.length-1; i++) {
+            //draw points of produkts on map
+            canvas.drawCircle(shortestPath[i][0], shortestPath[i][1], 18, paint);
+
+            //select next product x, y
             nextX = shortestPath[i][0];
             nextY = shortestPath[i][1];
+
          //   System.out.println(nextX + "next" + nextY);
+            //Find closest checkpoint to next product
             destinationCheckpoint = findClosestCheckpoint(nextX, nextY);
+
+            //Draw line from current checkpoint to closest checkpoint of new product.
             DrawLine(startX, startY, destinationCheckpoint[0][0], startY);
             DrawLine(destinationCheckpoint[0][0], startY, destinationCheckpoint[0][0], destinationCheckpoint[0][1]);
             startX = destinationCheckpoint[0][0];
